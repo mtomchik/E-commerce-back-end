90a656c3180a077f22126d9890235c37a4fcb2c7
Mtomchik <matthew.tomchik@yahoo.com>
2022-11-14 06:33:49 -0700

    started filling code

--- models/index.js
+++ models/index.js
@@ -5,8 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 
